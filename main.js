f7a19f200c8a5e1b86c5b4f0295ec18f7740177c
nambook445 <nambook445@naver.com>
2021-12-21 17:08:05 +0900

    해결 이제 화면작업시작

--- main.js
+++ main.js
@@ -94,19 +94,19 @@ var app = http.createServer(function(request,response){
         response.writeHead(200);
         response.end(template);
         });
-    }
-        else if(pathname === '/create_process'){
-            var body = '';
-            request.on('data', function(data){
-                body = body + data;
-            });
-            request.on('end', function(){
-                var post = qs.parse(body);
-                var title = post.title;
-                var description = post.description
-                fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-                    response.writeHead(302, {Location: encodeURI(`/?id=${title}`)});
-                    response.end();
+      }
+      else if(pathname === '/create_process'){
+        var body = '';
+        request.on('data', function(data){
+          body = body + data;
+        });
+        request.on('end', function(){
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302,{Location: encodeURI(`/?id=${title}`)});
+          response.end();
                 })
             });
         } else if(pathname === '/update'){
